cd7b8a821463602f9bd7c566feb059fdee140566
Aswin Krishnamoorthy <winnyboy5@gmail.com>
2024-10-06 11:49:34 +0000

    margin-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { Card, CardBody } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <Card className="max-w-md mx-auto mt-4">
+    <Card className="max-w-md mx-auto mt-10">
       <CardBody className="text-center">
         <h1 className="text-5xl">Next.js Boiler</h1>
-        <p className="text-xl">A simple Next.js boilerplate</p>
+        <p className="text-xl mt-5">A simple Next.js boilerplate</p>
       </CardBody>
     </Card>
   );
